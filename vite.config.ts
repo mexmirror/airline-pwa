01d62a33ea2c5f0a9a0815ef4818ec7fa65a6daf
Oliviero Chiodo <oliviero.chiodo@zuehlke.com>
2023-07-14 17:31:27 +0200

    Add basic PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Oceanic Airlines - Boarding Pass",
+        short_name: "Oceanic Air",
+        description: "Your assistant through the journey of your travel.",
+        theme_color: "#ffffff",
+        start_url: "/",
+        icons: [
+          {
+            src: "192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
